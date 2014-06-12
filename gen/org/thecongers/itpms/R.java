d37213913b203446892b828b680dc6d8f28cf45d
Keith Conger <keith.conger@gmail.com>
2014-06-11 21:57:41 -0400

    * Get iTPMS MAC from paired list * Added SVG graphics

--- gen/org/thecongers/itpms/R.java
+++ gen/org/thecongers/itpms/R.java
@@ -17,35 +17,41 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
-        public static final int txtOutput=0x7f090000;
+        public static final int action_settings=0x7f0a0004;
+        public static final int imageView1=0x7f0a0001;
+        public static final int imageView2=0x7f0a0002;
+        public static final int linearLayout1=0x7f0a0000;
+        public static final int txtOutput=0x7f0a0003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
+    }
+    public static final class raw {
+        public static final int ui=0x7f050000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int front_alert_settings_title=0x7f060002;
-        public static final int pref_front_high_pressure_name=0x7f060005;
-        public static final int pref_front_high_pressure_summary=0x7f060006;
-        public static final int pref_front_low_pressure_name=0x7f060003;
-        public static final int pref_front_low_pressure_summary=0x7f060004;
-        public static final int pref_rear_high_pressure_name=0x7f06000a;
-        public static final int pref_rear_high_pressure_summary=0x7f06000b;
-        public static final int pref_rear_low_pressure_name=0x7f060008;
-        public static final int pref_rear_low_pressure_summary=0x7f060009;
-        public static final int rear_alert_settings_title=0x7f060007;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int front_alert_settings_title=0x7f070002;
+        public static final int pref_front_high_pressure_name=0x7f070005;
+        public static final int pref_front_high_pressure_summary=0x7f070006;
+        public static final int pref_front_low_pressure_name=0x7f070003;
+        public static final int pref_front_low_pressure_summary=0x7f070004;
+        public static final int pref_rear_high_pressure_name=0x7f07000a;
+        public static final int pref_rear_high_pressure_summary=0x7f07000b;
+        public static final int pref_rear_low_pressure_name=0x7f070008;
+        public static final int pref_rear_low_pressure_summary=0x7f070009;
+        public static final int rear_alert_settings_title=0x7f070007;
     }
     public static final class style {
         /** 
@@ -69,11 +75,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class xml {
         public static final int user_settings=0x7f040000;
--- src/org/thecongers/itpms/MainActivity.java
+++ src/org/thecongers/itpms/MainActivity.java
@@ -1,20 +1,30 @@
 package org.thecongers.itpms;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Set;
 import java.util.UUID;
- 
+
 import org.thecongers.itpms.R;
 import org.thecongers.itpms.UserSettingActivity;
- 
+
+import com.caverock.androidsvg.SVG;
+import com.caverock.androidsvg.SVGParseException;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.PictureDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -22,6 +32,9 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
  
@@ -40,12 +53,13 @@ public class MainActivity extends Activity {
   private BluetoothSocket btSocket = null;
   
   private ConnectedThread mConnectedThread;
+  private Set<BluetoothDevice>pairedDevices;
    
   // SPP UUID service
   private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
  
   // MAC-address of iTPMS
-  private static String address = "00:11:67:E5:E4:66";
+  private String address;
    
   /** Called when the activity is first created. */
   @SuppressLint("HandlerLeak")
@@ -54,6 +68,34 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
     setContentView(R.layout.activity_main);
+    
+    // Keep screen on
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    
+    String svgUI = readRawTextFile(this, R.raw.ui);
+    String svgFUILive = svgUI.replaceAll("PP", "**");
+    svgFUILive = svgFUILive.replaceAll("TTT", "***");
+    svgFUILive = svgFUILive.replaceAll("VVV", "***");
+    final ImageView  imageView = (ImageView) findViewById(R.id.imageView1);
+    imageView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    
+    String svgRUILive = svgUI.replaceAll("PP", "**");
+    svgRUILive = svgRUILive.replaceAll("TTT", "***");
+    svgRUILive = svgRUILive.replaceAll("VVV", "***");
+    final ImageView  imageView2 = (ImageView) findViewById(R.id.imageView2);
+    imageView2.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    try
+    {
+       SVG svgF = SVG.getFromString(svgFUILive);
+       SVG svgR = SVG.getFromString(svgRUILive);
+       Drawable drawableF = new PictureDrawable(svgF.renderToPicture());
+       Drawable drawableR = new PictureDrawable(svgR.renderToPicture());
+       imageView.setImageDrawable(drawableF);
+       imageView2.setImageDrawable(drawableR);
+    }
+    catch(SVGParseException e)
+    {}
+   
  
     txtOutput = (TextView) findViewById(R.id.txtOutput);
     
@@ -75,7 +117,7 @@ public class MainActivity extends Activity {
                 	sbhex.append(hexData[i]);
                 }
                 // Get sensor position
-                int position = Integer.parseInt(hexData[3],16);
+                String position = hexData[3];
                 // Get sensor ID
                 StringBuilder sensorID = new StringBuilder();
                 sensorID.append(hexData[4]);
@@ -83,7 +125,7 @@ public class MainActivity extends Activity {
                 sensorID.append(hexData[6]);
                 sensorID.append(hexData[7]);
                 // Get temperature
-                int tempC = Integer.parseInt(hexData[8],16);
+                int tempC = Integer.parseInt(hexData[8],16) - 50;
                 double tempF = (9.0/5.0) * tempC + 32.0;              
                 // Get tire pressure
                 int psi = Integer.parseInt(hexData[9],16);
@@ -97,10 +139,41 @@ public class MainActivity extends Activity {
                 double voltage = battery;
                 // Get checksum
                 String checksum = hexData[11];
+                Log.d(TAG, "Sensor ID: " + sensorID.toString() + ", Sensor Position: " + position + ", Temperature(F): " + String.valueOf(tempF) + ", Pressure: " + String.valueOf(psi) + ", Voltage: " + String.valueOf(voltage) + ", Checksum: " + checksum +", Data: " + sbhex.toString() +  ", Bytes:" + msg.arg1);
+                if (sensorID.toString().contains("1E9D533E")){
+                	String svgFUI = readRawTextFile(MainActivity.this, R.raw.ui);
+                    String svgFUILive = svgFUI.replaceAll("PP", String.valueOf(psi));
+                    svgFUILive = svgFUILive.replaceAll("TTT", String.valueOf(tempF));
+                    svgFUILive = svgFUILive.replaceAll("VVV", String.valueOf(voltage));
+                    try
+                    {
+                       SVG svg = SVG.getFromString(svgFUILive);
+                       Drawable drawable = new PictureDrawable(svg.renderToPicture());
+                       imageView.setImageDrawable(drawable);
+                    }
+                    catch(SVGParseException e)
+                    {}
+                	
+                } else if (sensorID.toString().contains("1E9D4899")){
+                	Log.d(TAG, "Matched");
+                	String svgRUI = readRawTextFile(MainActivity.this, R.raw.ui);
+                    String svgRUILive = svgRUI.replaceAll("PP", String.valueOf(psi));
+                    svgRUILive = svgRUILive.replaceAll("TTT", String.valueOf(tempF));
+                    svgRUILive = svgRUILive.replaceAll("VVV", String.valueOf(voltage));
+                    try
+                    {
+                       SVG svg = SVG.getFromString(svgRUILive);
+                       Drawable drawable = new PictureDrawable(svg.renderToPicture());
+                       imageView2.setImageDrawable(drawable);
+                    }
+                    catch(SVGParseException e)
+                    {}
+                	
+                }
         
                 // Display
-                txtOutput.append("Sensor ID: " + sensorID.toString() + ", Sensor Position: " + String.valueOf(position) + ", Temperature(F): " + String.valueOf(tempF) + ", Pressure: " + String.valueOf(psi) + ", Voltage: " + String.valueOf(voltage) + ", Data: " + sbhex.toString() + "\n");
-            	Log.d(TAG, "Sensor ID: " + sensorID.toString() + ", Sensor Position: " + String.valueOf(position) + ", Temperature(F): " + String.valueOf(tempF) + ", Pressure: " + String.valueOf(psi) + ", Voltage: " + String.valueOf(voltage) + ", Data: " + sbhex.toString() +  ", Bytes:" + msg.arg1);
+                
+                txtOutput.append("Sensor ID: " + sensorID.toString() + ", Sensor Position: " + position + ", Temperature(F): " + String.valueOf(tempF) + ", Pressure: " + String.valueOf(psi) + ", Voltage: " + String.valueOf(voltage) + ", Data: " + sbhex.toString() + "\n");
             	break;
     		}
         };
@@ -108,6 +181,18 @@ public class MainActivity extends Activity {
      
     btAdapter = BluetoothAdapter.getDefaultAdapter();		// get Bluetooth adapter
     checkBTState();
+    pairedDevices = btAdapter.getBondedDevices();
+    // If there are paired devices
+	   if (pairedDevices.size() > 0) {
+		   // Loop through paired devices
+		   for (BluetoothDevice device : pairedDevices) {
+			   if (device.getName().contains("iTPMS")) {
+				   address = device.getAddress();
+				   Log.d(TAG, "Device found: " + device.getName() + " " + device.getAddress());
+				   
+			   }
+		   }
+	   }
   }
   
   private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
@@ -128,48 +213,56 @@ public class MainActivity extends Activity {
  
     Log.d(TAG, "...onResume - try connect...");
    
-    // Set up a pointer to the remote node using it's address.
-    BluetoothDevice device = btAdapter.getRemoteDevice(address);
-   
-    // Two things are needed to make a connection:
-    //   A MAC address, which we got above.
-    //   A Service ID or UUID.  In this case we are using the
-    //     UUID for SPP.
-    
-	try {
-		btSocket = createBluetoothSocket(device);
-	} catch (IOException e) {
-		errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
-	}
-    
-    /*try {
-      btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
-    } catch (IOException e) {
-      errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
-    }*/
-   
-    // Discovery is resource intensive.  Make sure it isn't going on
-    // when you attempt to connect and pass your message.
-    btAdapter.cancelDiscovery();
-   
-    // Establish the connection.  This will block until it connects.
-    Log.d(TAG, "...Connecting...");
-    try {
-      btSocket.connect();
-      Log.d(TAG, "....Connection ok...");
-    } catch (IOException e) {
-      try {
-        btSocket.close();
-      } catch (IOException e2) {
-        errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
-      }
+    if (address != null){
+    	// Set up a pointer to the remote node using it's address.
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+       
+        // Two things are needed to make a connection:
+        //   A MAC address, which we got above.
+        //   A Service ID or UUID.  In this case we are using the
+        //     UUID for SPP.
+        
+    	try {
+    		btSocket = createBluetoothSocket(device);
+    	} catch (IOException e) {
+    		errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+    	}
+        
+        /*try {
+          btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+          errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }*/
+       
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        btAdapter.cancelDiscovery();
+       
+        // Establish the connection.  This will block until it connects.
+        Log.d(TAG, "...Connecting...");
+        try {
+          btSocket.connect();
+          Log.d(TAG, "....Connection ok...");
+        } catch (IOException e) {
+          try {
+            btSocket.close();
+          } catch (IOException e2) {
+            errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+          }
+        }
+         
+        // Create a data stream so we can talk to server.
+        Log.d(TAG, "...Create Socket...");
+       
+        mConnectedThread = new ConnectedThread(btSocket);
+        mConnectedThread.start();
+    	
+    } else {
+    	Toast.makeText(MainActivity.this,
+				"No iTPMS found paired!",
+				Toast.LENGTH_LONG).show();
     }
-     
-    // Create a data stream so we can talk to server.
-    Log.d(TAG, "...Create Socket...");
-   
-    mConnectedThread = new ConnectedThread(btSocket);
-    mConnectedThread.start();
+    
   }
  
   @Override
@@ -189,7 +282,7 @@ public class MainActivity extends Activity {
     // Check for Bluetooth support and then check to make sure it is turned on
     // Emulator doesn't support Bluetooth and will return null
     if(btAdapter==null) { 
-      errorExit("Fatal Error", "Bluetooth not support");
+      errorExit("Fatal Error", "Bluetooth not supported");
     } else {
       if (btAdapter.isEnabled()) {
         Log.d(TAG, "...Bluetooth ON...");
@@ -277,4 +370,24 @@ public class MainActivity extends Activity {
               return super.onOptionsItemSelected(item);
   	}
   }
+  //Read raw text file
+  public static String readRawTextFile(Context ctx, int resId)
+  {
+	    InputStream inputStream = ctx.getResources().openRawResource(resId);
+
+	    InputStreamReader inputreader = new InputStreamReader(inputStream);
+	    BufferedReader buffreader = new BufferedReader(inputreader);
+	    String line;
+	    StringBuilder text = new StringBuilder();
+
+	    try {
+	        while (( line = buffreader.readLine()) != null) {
+	            text.append(line);
+	            text.append('\n');
+	        }
+	    } catch (IOException e) {
+	        return null;
+	    }
+	    return text.toString();
+	}
 }
